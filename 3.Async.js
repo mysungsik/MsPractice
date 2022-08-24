118f8c1c0e02b6caf2e7124df21a7f806a22abec
cms <audtlr93@naver.com>
2022-08-24 23:05:08 +0900

    async, promise , async await

--- 3.Async.js
+++ 3.Async.js
@@ -1,11 +1,27 @@
-const fs = require("fs")
+// 비동기식 작동
 
-function readFile(){
-    const data = fs.readFileSync("3.AsyncData.txt")
-    console.log(data.toString()) 
+const fs = require("fs/promises")
+
+function readFiles(){
+    let fileData;
+    fs.readFile("3.AsyncData.txt", function(error,fileData){
+        console.log(fileData.toString())
+    })
 }
-readFile();
 
-// 문자열을 string 으로 변환 => toString
-//  [데이터 파일이 아님을 알것.]
+// 비동기식 작동 - 콜백함수지옥 탈출하기 [ pormise, .then(funtion)]
 
+function readFilesToPromise(){
+    let fileData;
+    fs.readFile("3.AsyncData.txt").then(function(fileData){
+        console.log(fileData.toString())
+        console.log("file sync done!")
+    })
+    console.log("hi there!")
+}
+
+// .then을 무한히 연결하여 [콜백함수의 지옥에서 탈출하여, 깔끔한 코드를 만들 수 있다.]
+
+readFilesToPromise()
+
+// 프로미스 안에서 사용하ㅣㄹ await
