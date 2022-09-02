fea80f8acf8bd829906c869e0838825233033e4b
cms <audtlr93@naver.com>
2022-09-02 14:45:50 +0900

    noSQL mongoDB make blog post + make arrangement system with qurey

--- routes/blog.js
+++ routes/blog.js
@@ -23,7 +23,42 @@ router.get('/posts', async function(req, res) {
   //  [필요한 데이터만을 불러와서, 서버에 무리가 없게 하자!]
   const fromDatabase = await db.getDb().collection("posts").find({},{title:1 , summary:1, "author.name" :1}).toArray()
 
-  res.render('posts-list',{fromDatabase:fromDatabase});
+
+  // 두가지 옵션을 가진, 정렬을 만들어보았다.
+
+  let authororder = req.query.authororder;
+  let titleorder = req.query.titleorder;
+
+  let orderdata = "right"
+
+  if(titleorder == "right" || authororder == "right"){
+    orderdata = "left"
+  }
+
+  if( !authororder){
+    fromDatabase.sort(function(A,B){
+      if(orderdata =="right" && A.title > B.title){
+        return 1 
+      }
+      else if(orderdata =="left" && B.title > A.title){
+        return 1
+      }
+      return -1
+    })
+  }
+  else if(!titleorder){
+    fromDatabase.sort(function(A,B){
+      if(orderdata =="right" && A.author.name > B.author.name){
+        return 1 
+      }
+      else if(orderdata =="left" && B.author.name > A.author.name){
+        return 1
+      }
+      return -1
+    })
+  }
+
+  res.render('posts-list',{fromDatabase:fromDatabase, orderdata:orderdata});
 });
 
 
@@ -47,27 +82,77 @@ router.post("/posts", async function(req,res){
   email: author.email } 
   }
 
-  const result = await db.getDb().collection("posts").insertOne(newPost)
-  console.log(result)
+  await db.getDb().collection("posts").insertOne(newPost)
+
   res.redirect("/posts")
 
   // 결론 ==> 다른 컬렉션을 가져오고싶으면, [병합]이 아니라 [다른컬렉션을 한번 더 쿼리해라]
 })
 
 router.get("/posts/:id", async function(req,res){
-  
-  let pageId = ObjectId(req.params.id) 
-  // [받아온 아이디를 데이터베이스로 보낼때에는] [반드시 ObjectId를 사용하여 변환시킬것]
 
-  const fromDatabase = await db.getDb().collection("posts").findOne({_id: pageId})
-  res.render("post-detail", {fromDatabase:fromDatabase})
+  // [받아온 아이디를 데이터베이스로 보낼때에는] [반드시 ObjectId를 사용하여 변환시킬것]
+  
+  // [오류제어]
+  // [try catch]로 [혹시 주소를 잘못입력하여 [데이터베이스에서 해당_id 값으로 데이터를 찾을 수 없는 경우] ]
+  //    mySQL 의 경우에는 [ 오류처리 미들웨어로 자연스럽게 넘어가지만] [noSQL]인 [mongoDB 에서는 수동으로 처리해주어야한다.]
+  let pageId;
+
+  try{
+    pageId = new ObjectId(req.params.id) 
+  }
+  catch(error){
+    res.status(404).render("404")
+  }
+
+  const fromDatabase = await db.getDb().collection("posts").findOne({_id: pageId}, {summary:0})
+
+  // [받아온 값을 수정하여, (데이터베이스에는 넣지 말고) [사용하기]]
+
+  const trasnformData = {
+    ...fromDatabase,
+    date : fromDatabase.date.toISOString(),
+    humandate : fromDatabase.date.toLocaleDateString("ko-KR",{
+      weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
+  }
+ // js toLocaleDateString [구글검색 - 사용법나옴]
+
+
+  if(!fromDatabase){
+    return res.status(404).render("404")
+  }
+  res.render("post-detail", {fromDatabase:trasnformData})
 })
+
+router.get("/posts/:id/Edit", async function(req,res){
+  let pageId = new ObjectId(req.params.id)
+  const fromDatabase = await db.getDb().collection("posts").findOne({_id:pageId}, {title:1, summary:1, body:1})
+
+  res.render("update-post", {fromDatabase:fromDatabase})
+})
+
+
+router.post("/posts/:id/Edit", async function(req,res){
+  let pageId = new ObjectId(req.params.id)
+
+  await db.getDb().collection("posts").updateOne({_id : pageId}, { $set : {title: req.body.title, summary : req.body.summary, body: req.body.content}} )
+
+  res.redirect("/posts")
+})
+
+router.get("/posts/:id/Delete", async function(req,res){
+  let pageId = new ObjectId(req.params.id)
+
+  await db.getDb().collection("posts").deleteOne({ _id: pageId})
+
+  res.redirect("/posts")
+})
+
+
 
 router.get('/new-post', async function(req, res) {
   const authors = await db.getDb().collection("authors").find().toArray()
   res.render('create-post', {authors:authors});
 });
-
-
 
 module.exports = router;
