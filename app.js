95cde3ee03e87fa1391ea8eb59507488b9214271
cms <audtlr93@naver.com>
2022-09-08 13:42:38 +0900

    authentication, aurthorziation finish

--- app.js
+++ app.js
@@ -30,7 +30,27 @@ app.use(session({                     // 5. 세션을 통과하는 미들웨어 
   resave : false,
   saveUninitialized : false,
   store : SessionStore
-}))     
+}))
+
+
+app.use(async function(req, res, next){             // 4-0 모든 요청에 처리되는 미들웨어를 생성
+
+  const user = req.session.user;                                              // 4-1 세션에 저장된 유저가 있다면(로그인했다면)
+  const isAuth = req.session.isAuthenticated;                                  // 4-2 인증서를 주고,
+
+  if(!user || !isAuth){
+    return next();                // 4-3 세션에 저장된 유저가 없다면(로그인하지 않았다면), 이 라우터(미들웨어) 를 넘어가라 [여기까지만 실행하고, 다음 라우트를 실행해라]
+  }
+
+  
+  const userDoc = await db.getDb().collection("users").findOne({_id : user.id})   //  4-4 데이터베이스에서, 인증한 유저와 같은 유저데이터를 찾아 저장
+  const isAdmin = userDoc.isAdmin;                                                //  4-4 그 유저가 관리자 인지도 파악  [ 미리 데이터베이스에 업데이트해서 admin = true 넣어둔 아이디가 있음]
+
+  res.locals.isAuth = isAuth;             // 4-5 인증이 되어있는지에 대하여 [ 전역변수처리(모든 라우트, 모든 템플릿까지! )]
+  res.locals.isAdmin = isAdmin;           // 4-5 관리자인지 아닌지에 대하여 [ 전역변수처리(모든 라우트, 모든 템플릿까지! )]
+
+  next()                                  // 4-6 다 되엇으면 다음 으로 넘어가게 만들어라! [안그러면 평생 로딩중임]
+})
 
 app.use(demoRoutes);
 
--- routes/demo.js
+++ routes/demo.js
@@ -16,19 +16,35 @@ router.get('/signup', function (req, res) {             // 3-3. 전부다 다시
   if(!req.session.userInputData){                       // 3-4. 만약 req.session.userInputData 가 없다면 (= 잘못입력한적이 없다면) ejs 로 보낼값은 "" 빈칸
     userInputData = {
       haserror : false,
+      message: "",
       email : "",
       confirmemail : "",
       password : ""
     }
   }
         
-  req.session.userInputData = null;                     // 3-5 세션에 데이터가 쌓이는것을 방지하기위해, 데이터 세션 초기화
+  req.session.userInputData = null;                     // 3-5  [데이터 세션 초기화], [세션에 데이터가 쌓이는것을 방지하기위해]
 
   res.render('signup', {userInputData:userInputData});
 });
 
 router.get('/login', function (req, res) {
-  res.render('login');
+
+  let inputUserData = req.session.userInputData;
+
+  if(!req.session.userInputData){
+    inputUserData ={
+      haserror : false,
+      message: "",
+      email : "",
+      password : ""
+    }
+  }
+  req.session.userInputData = null
+ 
+
+
+  res.render('login',{inputUserData:inputUserData});
 });
 
 router.post('/signup', async function (req, res) {
@@ -49,6 +65,7 @@ router.post('/signup', async function (req, res) {
 
     req.session.userInputData = {                       // 3-1. 잘못된 양식으로 회원가입을 하려고 했을 경우, 전부다 다시 쓰는것을 방지하기위한 , 데이터 세션 생성
       haserror : true,
+      message: "not correct form",
       email : enteredEmail,
       confirmemail : enteredCongfirmEmail,
       password : enteredPassword
@@ -64,13 +81,22 @@ router.post('/signup', async function (req, res) {
   const existingUsers = await db.getDb().collection("users").findOne({email:enteredEmail}) 
 
   if(existingUsers){
-    console.log("user already exists")
-    return res.redirect("/signup")
+    req.session.userInputData = {                       // 3-1. 잘못된 양식으로 회원가입을 하려고 했을 경우, 전부다 다시 쓰는것을 방지하기위한 , 데이터 세션 생성
+      haserror : true,
+      message: "User alrady exist",
+      email : enteredEmail,
+      confirmemail : enteredCongfirmEmail,
+      password : enteredPassword
+    }
+    req.session.save(function(){
+       res.redirect("/signup")
+    })
+    return                                                // return 을 밖으로 빼지 않으면, if 내부의 문장만 멈추고, 다음 코드로 넘어가버림
   }
 
   await db.getDb().collection("users").insertOne(user)
 
-  res.redirect("/signup")
+  res.redirect("/login")
 
 });
 
@@ -92,8 +118,17 @@ router.post('/login', async function (req, res) {
   const passwordIsEqual = await bcryptjs.compare(enteredPassword,existingUsers.password) // 1-2.bcryptjs의 내장 매소드로 [입력한 패스워드, 해싱된 데이터베이스의 패스워드] 를 서로 비교, 결과값은 [불리언]으로 나온다.
   
   if(!passwordIsEqual){
-    console.log("wrong password")
-    return res.redirect("/login")
+    
+    req.session.userInputData = {
+      haserror : true,
+      message: "incorrect user input",
+      email : enteredEmail,
+      password : enteredPassword
+    }
+    req.session.save(function(){
+       res.redirect("/login")
+    })
+    return
   }
 
   console.log("user is authenticated")
@@ -102,6 +137,7 @@ router.post('/login', async function (req, res) {
     id : existingUsers._id,       // 2-2. sessions 콜렉션 안에 session 이란 폴더 안에 id와 email
     email : existingUsers.email
   }
+  req.session.isAuthenticated = true
 
   req.session.save(function(){  // 2-3. 세션이 저장되면 admin으로 들어가지게 만듬
     res.redirect("/admin")
@@ -111,8 +147,8 @@ router.post('/login', async function (req, res) {
 
 
 router.get('/admin', function (req, res) {
-  if(!req.session.user){
-    res.status(401).render("401")     // 2-4. 세션이 없으면, /admin  접근불가
+  if(!req.session.isAuthenticated){ //    or !req.session.user [세션이 없다면]
+    return res.status(401).render("401")     // 2-4. 인증이 false라면 (= or 세션이 없으면), /admin  접근불가
   }
 
   res.render('admin');
@@ -122,6 +158,7 @@ router.get('/admin', function (req, res) {
 
 router.post('/logout', function (req, res) {
   req.session.user = null   // 2-5. 세션을 전부 지우진 않고, 세션 안의 user 데이터만 지움
+  req.session.isAuthenticated = false;
 
   res.redirect("/login")
 });
